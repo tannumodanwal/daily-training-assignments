4dbb9f6b926abf5bcc737800bdefe7f1c5ea5a88
Tannu Modanwal <tannumodanwal09@gmail.com>
2026-01-28 11:26:03 +0530

    oppositeTriangle

---
+++ SideTriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SideTriangle {
+    public static void main(String[] args) {
+        Scanner s  = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+              System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        for(int i = n-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
+              System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
